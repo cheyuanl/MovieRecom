dc4a6ff8ef8f827c559332198ad4b9d87b42ae42
cheyuanl <cheyuanl@andrew.cmu.edu>
2016-12-11 20:26:16 -0500

    add documentation

--- notebook/experiemnt_fm.py
+++ notebook/experiemnt_fm.py
@@ -1,11 +1,26 @@
 from pyfm import pylibfm
+from sklearn.metrics import mean_squared_error
 
-def iter_error(X_train, y_tr, X_test, y_te, iters = [1,3,5,10,25,50,100,200], lr = 1E-2, num_factor = 3):
+def iter_error(X_train, y_tr, X_test, y_te, iters = [1,3,5,10,25,50,100,200], lr = 1E-2, latent = 3):
+	""" Test the MSE error against number of iteration.
+		Args:
+			X_train (2d sparse matrix) : training data
+			y_tr (numpy array) training : target
+			X_test (2d sparse matrix) : testing data
+			y_te (numpy arry) : testing target
+			iters (list) : number of iterations
+			lr (float) : learning rate
+			num_factors : number of laten variable
+		Result:
+			models (fm model) : factorization machine model
+			train_error (list of tuple) : [(k, error), ...]
+			test_error (list of tuple) : [(k, error), ...]
+	"""
     models = []
     train_error, test_error = [], []
 
     for k in iters:
-        model = pylibfm.FM(num_factors=num_factor, num_iter=k,
+        model = pylibfm.FM(num_factors=latent, num_iter=k,
                         verbose=True, task="regression",
                         initial_learning_rate=lr, learning_rate_schedule="optimal")
         model.fit(X_train, y_tr)
@@ -20,11 +35,25 @@ def iter_error(X_train, y_tr, X_test, y_te, iters = [1,3,5,10,25,50,100,200], lr
     return models, train_error, test_error
 
 
-def laten_error(X_train, y_tr, X_test, y_te, laten = [1,3,5,10,25,50,100,200], lr = 1E-2 , iters = 100):
+def latent_error(X_train, y_tr, X_test, y_te, latent = [1,3,5,10,25,50,100,200], lr = 1E-2 , iters = 100):
+	""" Test the MSE error against number of latent factor.
+	Args:
+		X_train (2d sparse matrix) : training data
+		y_tr (numpy array) training : target
+		X_test (2d sparse matrix) : testing data
+		y_te (numpy arry) : testing target
+		iters (list) : number of iterations
+		lr (float) : learning rate
+		num_factors : number of laten variable
+	Result:
+		models (fm model) : factorization machine model
+		train_error (list of tuple) : [(k, error), ...]
+		test_error (list of tuple) : [(k, error), ...]
+	"""
     models = []
     train_error, test_error = [], []
 
-    for k in laten:
+    for k in latent:
         model = pylibfm.FM(num_factors=k, num_iter=iters,
                         verbose=True, task="regression",
                         initial_learning_rate=lr, learning_rate_schedule="optimal")
