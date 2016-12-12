815ee5124b089238b8e0fc318a07674e3a44518f
cheyuanl <cheyuanl@andrew.cmu.edu>
2016-12-11 19:30:20 -0500

    tokenizer

---
+++ notebook/experiemnt_fm.py
@@ -0,0 +1,20 @@
+from pyfm import pylibfm
+
+def iter_error(X_train, y_tr, X_test, y_te, iters = [1,3,5,10,25,50,100,200], lr = 1E-2, num_factor = 3):
+    models = []
+    train_error, test_error = [], []
+
+    for k in iters:
+        model = pylibfm.FM(num_factors=num_factor, num_iter=k,
+                        verbose=True, task="regression",
+                        initial_learning_rate=lr, learning_rate_schedule="optimal")
+        model.fit(X_train, y_tr)
+        models.append((k, model))
+
+        pred = model.predict(X_train)
+        train_error.append((k, mean_squared_error(y_tr, pred)))
+
+        pred = model.predict(X_test)
+        test_error.append((k, mean_squared_error(y_te, pred)))
+
+    return models, train_error, test_error
--- notebook/tokenizer.py
+++ notebook/tokenizer.py
@@ -1,36 +1,37 @@
-def tokenize(jointTable):
+def tokenize(jointTable, features = ['all']):
     data, y = [], []
+    if 'all' in features:
+        features = ['genres', 'occupation', 'age', 'release_year', 'votes', 'age', 'imdbId', 'userId']
+
     for index, row in jointTable.iterrows():
-
         feature_dict = {}
-
-        # # Genres
-        # for cate in row['genres'].split(','):
-        #     key = 'category={}'.format(cate)
-        #     feature_dict[key] = 1
+        if 'genres' in features:
+            for cate in row['genres'].split(','):
+                key = 'category={}'.format(cate)
+                feature_dict[key] = 1
             
-        # # Occupation
-        # for cate in row['occupation'].split(','):
-        #     key = 'occupation={}'.format(cate)
-        #     feature_dict[key] = 1
+        if 'occupation' in features:
+            for cate in row['occupation'].split(','):
+                key = 'occupation={}'.format(cate)
+                feature_dict[key] = 1
 
-        # # Age 
-        # feature_dict['age'] = float(row['age'])
+        if 'age' in features:
+            feature_dict['age'] = float(row['age'])
+
+        if 'release_year' in features:
+            feature_dict['release_year'] = float(row['year'])
         
-        # # Release year
-        # feature_dict['release_year'] = float(row['year'])
+        if 'votes' in features:
+            feature_dict['votes'] = float(row['votes'])
         
-        # # Votes
-        # feature_dict['votes'] = float(row['votes'])
+        if 'age' in features:
+            feature_dict['gender'] = str(row['gender'])
         
-        # # Gender
-        # feature_dict['gender'] = str(row['gender'])
-        
-        # ImdbId
-        feature_dict['imdbId'] = str(row['imdbId'])
-        
-        # UserId
-        feature_dict['userId'] = str(row['userId'])
+        if 'imdbId' in features:
+            feature_dict['imdbId'] = str(row['imdbId'])
+
+        if 'userId' in features:
+            feature_dict['userId'] = str(row['userId'])
 
         data.append(feature_dict)
         y.append(float(row['rating']))
